f9b7b931094d03df7581d8da1c89ed0665e3eb0c
ayodelefasulu <fasuluayodele@gmail.com>
2024-10-04 01:50:07 +0100

    task complete

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 """
-Making change
+Given a pile of coins of different values, determine the fewest number of coins
+needed to meet a given amount total.
 """
 
 
 def makeChange(coins, total):
     """
-    Determines the fewest number of coins needed to meet a given total.
-
-    :param coins: A list of the coin denominations.
-    :param total: The total amount of money we want to make change for.
-    :return: The fewest number of coins needed to make change for total.
-        Returns -1 if it is not possible.
+    Prototype: def makeChange(coins, total)
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    coins is a list of the values of the coins in your possession
+    The value of a coin will always be an integer greater than 0
+    You can assume you have an infinite number of each denomination of coin
+    in the list
+    Your solution’s runtime will be evaluated in this task
     """
     if total <= 0:
         return 0
 
-    # Initialize DP array with a high value (greater than any possible coin
-    # count)
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Zero coins needed to make total 0
+    coins.sort(reverse=True)
+    count = 0
+    remaining_total = total
 
     for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
+        while remaining_total >= coin:
+            remaining_total -= coin
+            count += 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if remaining_total == 0:
+        return count
+    else:
+        return -1
