1fac230c2e4856ab418239cb6bd0bcededcaf2f7
ayodelefasulu <fasuluayodele@gmail.com>
2024-10-04 01:39:39 +0100

    task complete

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given total.
+
+    :param coins: A list of the coin denominations.
+    :param total: The total amount of money we want to make change for.
+    :return: The fewest number of coins needed to make change for total.
+        Returns -1 if it is not possible.
+    """
+    if total <= 0:
+        return 0
+
+    # Initialize DP array with a high value (greater than any possible coin
+    # count)
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # Zero coins needed to make total 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
